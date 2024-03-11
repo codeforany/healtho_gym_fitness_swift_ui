e81a5651c77e689bcb8a686bef63056a4440d27f
codeforany <schoolmaya5@gmail.com>
2024-03-12 01:11:32 +0530

    Initial Commit

---
+++ Healtho Gym/Healtho_GymApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healtho_GymApp.swift
+//  Healtho Gym
+//
+//  Created by CodeForAny on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Healtho_GymApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
