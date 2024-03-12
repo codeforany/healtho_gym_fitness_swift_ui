5445aca628bb9ddd18dfbdb1136bcd4e67a1875e
codeforany <schoolmaya5@gmail.com>
2024-03-13 00:42:03 +0530

    app setup & on boarding screen done

---
+++ Healtho Gym/Screen/Login/OnBoardingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingScreen.swift
+//  Healtho Gym
+//
+//  Created by CodeForAny on 12/03/24.
+//
+
+import SwiftUI
+
+struct OnBoardingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingScreen()
+}
---
+++ Healtho Gym/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  Healtho Gym
+//
+//  Created by CodeForAny on 12/03/24.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
